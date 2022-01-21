f6e4d3b59bedfb66fb732763e8d588fabdbdefcb
meriem <meriembenismail@insat.u-carthage.tn>
2022-01-20 16:01:10 -0800

    blogs

--- src/components/blogCard/BlogCard.jsx
+++ src/components/blogCard/BlogCard.jsx
@@ -1,16 +1,49 @@
 import React from 'react';
 import classes from './blogCard.module.css';
 import img from './gym7.jpg';
-import Button from '../button/Button';
+import Button from '../UI/button/Button';
 import DateBox from '../dateBox/DateBox';
-
+import {motion} from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+import { useAnimation } from 'framer-motion';
 export default function BlogCard() {
+  const {ref, inView} = useInView({
+    threshold: 0.2//20% should be visible
+  });
+  //ref : element that we want to monitor : when ref in view => inView =true , else false
+  const animation = useAnimation();
+  useEffect(() =>{
+     
+    // 3 hooks
+    if(inView) {
+      animation.start({
+      
+        x:0,
+        scale: 1,
+        transition: {
+          type: 'spring' , duration: 1,bounce:-0.5
+        }
+      });
+    }
+    if(!inView) {
+      animation.start({
+      x:"100vw",
+      scale: 0.3,
+      transition: {
+        type: 'spring' , duration: 1,bounce: 0
+      }
+    })
+    }
+      
+  },[inView]);
   return (
-      <div className={classes.card}>
+    <div ref={ref}>
+       <motion.div animate={animation} className={classes.card}>
         <div className={classes.img}>
           <div className={classes.date}>  <DateBox /></div>
          
-         <img src={img} alt="" />
+          <img src={img} alt="" />
 
         </div>
         
@@ -27,6 +60,9 @@ export default function BlogCard() {
         </div>
         
        
-      </div>
+      </motion.div>
+
+    </div>
+     
   );
 }
--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,21 +1,91 @@
 import React from 'react';
 import BlogCard from '../../components/blogCard/BlogCard';
 import BlogHeader from '../../components/blogHeader/BlogHeader';
-import Navbar from '../../components/navbar/Navbar';
+import Navbar from '../../components/layout/navbar/Navbar';
 import classes from './blogs.module.css';
-
+import Container from '../../components/layout/container/Container';
+import SearchBox from '../../components/UI/searchBox/SearchBox';
+import Title from '../../components/UI/title/Title';
+import {motion} from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+import { useAnimation } from 'framer-motion';
 export default function Blogs() {
+  
+
+  const {ref, inView} = useInView({
+    threshold: 0//20% should be visible
+  });
+  //ref : element that we want to monitor : when ref in view => inView =true , else false
+  const animation = useAnimation();
+  useEffect(() =>{
+     
+    // 3 hooks
+    if(inView) {
+      animation.start({
+      
+        x:0,
+        transition: {
+          type: 'spring' , duration: 1.5,bounce: 0
+        }
+      });
+    }
+    if(!inView) {
+      animation.start({x:"-100vw",
+      transition: {
+        type: 'spring' , duration: 1.5,bounce: 0
+      }
+    })
+    }
+      
+  },[inView]);
+
   return (
-      <div>
+   
+      <Container>
+         <div className={classes.blogs}>
           <Navbar />
-          <BlogHeader />
-          <div className={classes.cards}>
-            <BlogCard />
-            <BlogCard />
+            <BlogHeader />
 
-          </div>
+
+            <div className={classes.section}>
+              <div  style={{color:"white"}} className={classes.margin_section}>
+                <div className={classes.search}>
+                  <Title title="Search" width="200px" />
+                  <SearchBox />
+
+                </div>
+                <div ref={ref} className={classes.search}>
+                  <Title title="Catégories" width="260px"/>
+                  <motion.div animate={animation}>
+                    <h4 className={classes.cat}>Fitness</h4>
+                    <hr />
+                    <h4 className={classes.cat}>Gym</h4>
+
+                  </motion.div>
+                 
+
+                </div>
+                
+
+              </div>
+              <div className={classes.cards}>
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+              </div>
+
+            </div>
+            
+           
           
-
       </div>
+
+      </Container>
+      
+
+    
+     
   );
 }
